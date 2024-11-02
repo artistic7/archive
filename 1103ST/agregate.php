3096cf01da989d556defe08bcc7413a9e66863c6
Artistic Software <artisticsoftware7@gmail.com>
2024-11-02 04:55:01 +0000

    ...

---
+++ 1103ST/agregate.php
@@ -0,0 +1,71 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 3',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 1',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 3, 9',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 2',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 4',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 2',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 3, 10',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 1, 10',
+		'win(union 793)' => '1, 3, 5, 10',
+		'unions($10)' => '1, 3, 5, 8, 10',
+		'union + favorites' => '1, 3, 5, 8, 10',
+		'win(union 794)' => '1, 3, 8, 10',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 3',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 1, 4',
+		'win(union 6)' => '1, 4, 10, 11',
+		'unions($10)' => '1, 4, 10, 11',
+		'union + favorites' => '1, 4, 10, 11',
+	],
+];
