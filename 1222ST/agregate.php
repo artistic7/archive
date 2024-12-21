9820148219a5fe99346bfc51fe659d49b3c3ab6e
Artistic Software <artisticsoftware7@gmail.com>
2024-12-21 04:50:48 +0000

    ...

---
+++ 1222ST/agregate.php
@@ -0,0 +1,66 @@
+<?php
+
+return [
+	'1' => [
+		/**
+		Race 1
+		*/
+		'favorites' => '(F) 2, 5',
+	],
+	'2' => [
+		/**
+		Race 2
+		*/
+		'favorites' => '(F) 2, 14',
+	],
+	'3' => [
+		/**
+		Race 3
+		*/
+		'favorites' => '(F) 2',
+	],
+	'4' => [
+		/**
+		Race 4
+		*/
+		'favorites' => '(F) 11, 13',
+		'place(wp 1)' => '11',
+		'placesWP($30)' => '11',
+	],
+	'5' => [
+		/**
+		Race 5
+		*/
+		'favorites' => '(F) 2, 4',
+	],
+	'6' => [
+		/**
+		Race 6
+		*/
+		'favorites' => '(F) 1, 8',
+	],
+	'7' => [
+		/**
+		Race 7
+		*/
+		'favorites' => '(F) 2, 13',
+	],
+	'8' => [
+		/**
+		Race 8
+		*/
+		'favorites' => '(F) 1',
+	],
+	'9' => [
+		/**
+		Race 9
+		*/
+		'favorites' => '(F) 3, 11',
+	],
+	'10' => [
+		/**
+		Race 10
+		*/
+		'favorites' => '(F) 2',
+	],
+];
